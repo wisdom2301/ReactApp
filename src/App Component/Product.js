94ab39ddf3faadc202f5fd1a64dc908731c0003e
Wisdom2301 <freeborn.wisdom@gmail.com>
2021-08-06 09:53:36 +0100

    New commit

--- src/App Component/Product.js
+++ src/App Component/Product.js
@@ -3,18 +3,18 @@ import React, { useState, useEffect } from 'react';
 export default function Product() {
     const [count, setCount] = useState(0);
     
-    // const handleClick = () => {
+    const handleClick = () => {
 
         useEffect(() => {
           setCount (count + 1);  
         }, [])
         
-    // };
+    };
     return (
         <div>
             <p>{count}</p>
-            {/* {count ? <p>Too much money in the bag</p> : <p>You have no items in your cart</p>}
-            <button onClick = {handleClick}>Add to cart</button> */}
+            {count ? <p>Too much money in the bag</p> : <p>You have no items in your cart</p>}
+            <button onClick = {handleClick}>Add to cart</button>
         </div>
     )
 }
